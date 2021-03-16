b188790de3cb8680d435a22d609cd03a3d4fa845
KameharaShintarou <54792804+KameharaShintarou@users.noreply.github.com>
2021-03-16 16:23:45 +0900

    no message

---
+++ Aqua/Assets/Scripts/Goal.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    [SerializeField]
+    MeshRenderer MeshRenderer;
+
+    [SerializeField]
+    Material Material;
+
+    bool goalFlag;
+
+    void Start()
+    {
+        goalFlag = false;
+    }
+
+    public void OnGoalFlag()
+    {
+        goalFlag = true;
+        MeshRenderer.material = Material;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Player player = other.gameObject.GetComponent<Player>();
+
+            if (player.GetIsPlaying() &&
+                goalFlag)
+            {
+                // SceneChangeのスクリプトの関数に置き換えたい
+                SceneManager.LoadScene("Title");
+            }
+        }
+    }
+}
