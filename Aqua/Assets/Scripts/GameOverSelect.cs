7f394e932fa98b7f43d919be54096b91c3b6ca19
Yonesige <fukura2000h@outlook.jp>
2021-03-24 02:18:54 +0900

    Update-GameOver

---
+++ Aqua/Assets/Scripts/GameOverSelect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverSelect : MonoBehaviour
+{
+    // はじめに選択状態に設定するボタンを指定する。
+    [SerializeField]
+    private Button selectedButton = null;
+
+    public void Start()
+    {
+        // はじめに選択状態に設定
+        selectedButton.Select();
+    }
+
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("StageSelect");
+    }
+
+    public void OnClickRetryButton()
+    {
+        SceneManager.LoadScene("Stage");
+    }
+}
