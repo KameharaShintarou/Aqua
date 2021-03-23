d5881075776db1697f0e7dad6b5ed04704f8c214
Yonesige <fukura2000h@outlook.jp>
2021-03-23 13:40:07 +0900

    GameStart.cs

--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -4,15 +4,15 @@ using UnityEngine.UI;
 
 public class GameStart : MonoBehaviour
 {
-    //// はじめに選択状態に設定するボタンを指定する。
-    //[SerializeField]
-    //private Button selectedButton = null;
+    // はじめに選択状態に設定するボタンを指定する。
+    [SerializeField]
+    private Button selectedButton = null;
 
-    //public void Start()
-    //{
-    //    // はじめに選択状態に設定
-    //    selectedButton.Select();
-    //}
+    public void Start()
+    {
+        // はじめに選択状態に設定
+        selectedButton.Select();
+    }
 
     public void OnClickStartButton()
     {
