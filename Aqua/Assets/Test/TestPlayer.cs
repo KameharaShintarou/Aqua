ed6c1d46af4f114db1af4a9b5a21660932a6a889
Chikashi Nadoyama <chikashi.nadoyama@gmail.com>
2021-04-05 11:51:47 +0900

    [update]Load player motion

---
+++ Aqua/Assets/Test/TestPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour
+{
+    // 移動速度を指定します。
+    [SerializeField]
+    private float moveSpeed = 5;
+
+    // プレイヤーのAnimatorコンポーネントを指定します。
+    [SerializeField]
+    private Animator animator = null;
+
+    new Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ユーザー入力を取得
+        var vertical = Input.GetAxis("Vertical");
+
+        rigidbody.velocity = transform.forward * moveSpeed * vertical;
+
+        // AnimatorのSpeedパラメーターを設定
+        animator.SetFloat("Speed", Mathf.Abs(vertical));
+    }
+}
