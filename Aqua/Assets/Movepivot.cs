d4ea18cdc6725b251c67953c9c993b9858fa10d4
Yonesige <fukura2000h@outlook.jp>
2021-05-06 19:54:14 +0900

    Test

---
+++ Aqua/Assets/Movepivot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movepivot : MonoBehaviour
+{
+    private Vector3 target;
+
+    [SerializeField]
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Sin(Time.time)*speed+target.x, target.y, target.z);
+    }
+}
